20ae77537c965b2f203ad1cdfac9cc3e11c160f2
Afzal <kapadwalahajimohammedafzal@gmail.com>
2026-05-19 19:52:37 +0530

    feat: add login route to authentication controller

--- backend/src/routes/V1/auth.route.ts
+++ backend/src/routes/V1/auth.route.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import * as authController from "../../controllers/auth";
 
 const router = express.Router();
+
+router.post("/login", authController.logIn);
 
 export default router;
